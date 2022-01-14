3de81a758602e37b287704528355307af6d0f771
Heather Taylor <htaylor91@gmail.com>
2022-01-14 16:30:43 +0100

    Ajout de slideshow sur la page d'accueil (html, css, js), mise à jour de couleurs, nettoyage de certains commentaires.

--- navigation.js
+++ navigation.js
@@ -1,3 +1,4 @@
+/*Mobile navigation / hamburger button*/
 const hamburgerContainer = document.getElementById("hamburger-container");
 const navigationMenu = document.getElementById("navigation-menu");
         
@@ -5,3 +6,33 @@ hamburgerContainer.addEventListener('click', () =>
                                     {
                                         navigationMenu.classList.toggle('active');
                                     })
+
+
+/*Slideshow*/
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
